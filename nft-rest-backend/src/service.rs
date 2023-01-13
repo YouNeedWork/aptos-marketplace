a09ebb27cc72fd23baa4c31fab514b611f1f6220
wangyusong <admin@wangyusong.com>
2023-01-13 14:57:42 +0800

    setup step

--- nft-rest-backend/src/service.rs
+++ nft-rest-backend/src/service.rs
@@ -15,5 +15,15 @@ pub async fn fetch_nfts_on_market(_redis: Client, pg: db::DbPool) {
     )
     .unwrap();
 
+    // query the market items;
+
+    //select count(1) % 2 as onmarket,decoded_key from table_items where table_handle = '0xaeef8b346d7241bdc8db8b0ccc474b071e3b86f53e7dde459f154d1ea0554258' group by decoded_key;
+
+    //select * from table_items where decoded_key = '{"token_data_id": {"name": "Test Nft #16", "creator": "0xeebac1f5eea4446fd71e79a00660e91c03af910ef700f9f4d201f42c83a8f0d0", "collection": "Test Nft"}, "property_version": "0"}' and is_deleted = 'f' order by transaction_version desc limit 1;
+
+    // Query the detail for items;
+
+    // Save to redis.
+
     dbg!(&nfts);
 }
